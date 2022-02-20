1c53cdd45b5d6b7707a30ded5418483a64479ce5
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:47:25 -0800

    Update Red_Light_Green_Light.py

--- Red_Light_Green_Light.py
+++ Red_Light_Green_Light.py
@@ -1,12 +1,15 @@
 import random
 choice1 = random.randint(1, 2)
+
 if choice1 == 1:
     who = 'You got caught by the police for running a red light.'
+    
 else:
     who = 'You caused someone to swerve to avoid crashing into you and crash into a street light.'
 print("The goal of this game is to drive your car 10 yards with out runnning a red light. Good Luck!")
 
 distance = 10
+
 while distance > 0:
 
     print("You have " + str(distance) + " yards left to go.")
@@ -14,16 +17,21 @@ while distance > 0:
     light = "Yellow"
 
     choice = random.randint(1, 100)
+    
     if choice <= 25:
         light = 'Red'
+        
     else:
         light = 'Green'
 
     print(light + " Light!")
+    
     if move == "yes":
+        
         if light == "Red":
             print(who + " You have to go back to the start.")
             distance = 10
+            
         if light == "Green":
             distance -= 2
 
